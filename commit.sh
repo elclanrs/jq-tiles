211c1c35668d098bf0fea9919230312eacc2cd5a
elclanrs <elclanrs@gmail.com>
2012-10-17 05:21:19 -0400

    clean

--- commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cat jquery.tiles.js | uglifyjs -o jquery.tiles.min.js
-lessc -x jquery.tiles.less jquery.tiles.css
-echo 'Commit message: '
-read commit
-git commit -am "$commit"
-git push origin master
